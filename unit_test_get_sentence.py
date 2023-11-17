2482122222d5af636c2ed878155666ee654a8838
RahatLukuum <98338272+RahatLukuum@users.noreply.github.com>
2023-11-17 11:43:57 +0300

    Update unit_test_get_sentence.py

--- unit_test_get_sentence.py
+++ unit_test_get_sentence.py
@@ -9,16 +9,23 @@ def get_sentence():
     return sentence
 
 
-def test_get_sentence():  ## корректно ли функция get_sentence берёт предложения
-    sentences = ['sentence1', 'sentence2', 'sentence3', 'sentence with spaces', 'sentence with, and.']
+def test_get_sentence(personal_test):  ## корректно ли функция get_sentence берёт предложения
+    sentences = []
+    if personal_test != 0:
+        sentences = ['sentence1', 'sentence2', 'sentence3', 'sentence with spaces', 'sentence with, and.']
+    else:
+        n = int(input("Введите количество предложений, а затем напишите их по одному в строчке: "))
+        for i in range(n):
+            one_sent = input()
+            sentences.append(one_sent)
     sentence = get_sentence()
     if sentence in sentences:
         return 1
     else:
         return 0
 
-
-if test_get_sentence() == 1:
+personal_test = int(input("Если вы тестируете функцию, выбрав предложения самостоятельно напишите 0, иначе отличное от 0 число: "))
+if test_get_sentence(personal_test) == 1:
     print("OK")
 else:
     print("Error")
