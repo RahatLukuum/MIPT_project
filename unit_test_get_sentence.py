3f49eb09bee9cbc10162b400dd90b29a2456809a
RahatLukuum <98338272+RahatLukuum@users.noreply.github.com>
2023-11-17 11:08:41 +0300

    Update unit_test_get_sentence.py

--- unit_test_get_sentence.py
+++ unit_test_get_sentence.py
@@ -1,16 +1,16 @@
-# unit test
+# unit tests
 import random
 
 
 def get_sentence():
-    f = open('sentences.txt').read()
+    f = open('fortests.txt').read()
     sentences = f.split('\n')
     sentence = random.choice(sentences)
     return sentence
 
 
 def test_get_sentence():  ## корректно ли функция get_sentence берёт предложения
-    sentences = ['sentence1', 'sentence2', 'sentence3']
+    sentences = ['sentence1', 'sentence2', 'sentence3', 'sentence with spaces', 'sentence with, and.']
     sentence = get_sentence()
     if sentence in sentences:
         return 1
